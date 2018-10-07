3b1b736c7ac8b8d225d078181823c0feef5f83e6
Stephen Bunn <stephen@bunn.io>
2018-10-07 17:30:12 -0400

    Adding basic nested_empty_config test - Fixing small flake8 error
    Signed-off-by: Stephen Bunn <stephen@bunn.io>

--- tests/test_schema_builder.py
+++ tests/test_schema_builder.py
@@ -5,7 +5,8 @@ from hypothesis import given
 from hypothesis.strategies import characters
 
 import file_config
-from . import config, class_name
+
+from . import config, class_name, attribute_name
 
 
 @given(config(min_vars=0, max_vars=0))
@@ -45,22 +46,29 @@ def test_optional_var(config):
     assert "type" not in schema["properties"]["test"]
 
 
-@given(class_name(), characters())
-def test_var_title(config_name, title):
+@given(class_name(), characters(), characters())
+def test_var_metadata(config_name, title, description):
     config = file_config.make_config(
-        config_name, {"test": file_config.var(title=title)}
+        config_name, {"test": file_config.var(title=title, description=description)}
     )
     schema = file_config.build_schema(config)
     assert schema["properties"]["test"]["title"] == title
+    assert schema["properties"]["test"]["description"] == description
 
 
-@given(class_name(), characters())
-def test_var_description(config_name, description):
+@given(class_name(), attribute_name(), config(config_vars={}))
+def test_nested_empty_config(config_name, nested_config_name, nested_config):
+    print(nested_config)
     config = file_config.make_config(
-        config_name, {"test": file_config.var(description=description)}
+        config_name, {nested_config_name: file_config.var(nested_config)}
     )
     schema = file_config.build_schema(config)
-    assert schema["properties"]["test"]["description"] == description
+    assert len(schema["properties"]) == 1
+    assert len(schema["required"]) == 1
+    nested_config_schema = schema["properties"][nested_config_name]
+    assert nested_config_schema["type"] == "object"
+    assert len(nested_config_schema["properties"]) == 0
+    assert len(nested_config_schema["required"]) == 0
 
 
 @given(class_name())
