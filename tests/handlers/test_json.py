f5d7dc1a3671c0a837d0380cfc6eb4669e481fde
Stephen Bunn <stephen@bunn.io>
2018-10-16 13:51:04 -0400

    Adding a super basic json dumps test
    Signed-off-by: Stephen Bunn <stephen@bunn.io>

--- tests/handlers/test_json.py
+++ tests/handlers/test_json.py
@@ -1,9 +1,21 @@
 # Copyright (c) 2018 Stephen Bunn <stephen@bunn.io>
 # MIT License <https://opensource.org/licenses/MIT>
 
+import json
+import ujson
+
 from hypothesis import given
 from hypothesis.strategies import data
 
 import file_config
 
 from .. import config, builder
+
+
+
+@given(config(), data())
+def test_json_dumps(config, data):
+    instance = data.draw(builder.build_config(config))
+    json_content = instance.dumps_json(prefer="json")
+    assert isinstance(json_content, str)
+    json.loads(json_content)
