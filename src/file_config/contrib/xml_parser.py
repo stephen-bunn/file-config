dd05808a47cf6ed1b22db911e552f35876a62b4e
Stephen Bunn <stephen@bunn.io>
2018-11-15 18:26:01 -0500

    Adding docstrings for xml parsing
    Signed-off-by: Stephen Bunn <stephen@bunn.io>

--- src/file_config/contrib/xml_parser.py
+++ src/file_config/contrib/xml_parser.py
@@ -9,15 +9,32 @@ from lxml import etree
 
 @attr.s
 class XMLParser(object):
+    """ A custom XML parser which is reflective between xml and dictionaries.
+    """
 
     tree = attr.ib(type=etree.Element)
 
     @classmethod
     def _build_base(cls, element):
+        """ Builds a base value from the given etree element.
+
+        :param lxml.etree.Element element: The element to build from
+        :return: The built value
+        """
+
         return __builtins__[element.get("type")](element.text)
 
     @classmethod
     def _build_list(cls, list_element, dict_type=collections.OrderedDict):
+        """ Builds a list entry from a list parent element.
+
+        :param lxml.etree.Element list_element: The element to build from
+        :param class dict_type: The dictionary class to build with, defaults to
+            :class:`collections.OrderedDict`, optional
+        :return: The built list
+        :rtype: list
+        """
+
         result = []
         for element in list_element:
             if "type" in element.attrib:
@@ -31,6 +48,15 @@ class XMLParser(object):
 
     @classmethod
     def _build_dict(cls, root_element, dict_type=collections.OrderedDict):
+        """ Builds a dictionary entry from a dictionary parent element.
+
+        :param lxml.etree.Element root_element: The element to build from
+        :param dict_type: The dictionary class to build with, defaults to
+            :class:`collections.OrderedDict`, optional
+        :return: The built dictionary
+        :rtype: dict
+        """
+
         result = dict_type()
         for element in root_element:
             if "type" in element.attrib:
@@ -44,16 +70,33 @@ class XMLParser(object):
 
     @classmethod
     def _build_base_etree(cls, parent, key, value):
+        """ Builds a element from a base dictionary entry.
+
+        :param lxml.etree.Element parent: The parent element to add to
+        :param str key: The key of the entry
+        :param value: The value of th entry
+        :return: The built element
+        :rtype: lxml.etree.Element
+        """
+
         element = etree.Element(key, type=type(value).__name__)
         element.text = str(value)
         return element
 
     @classmethod
     def _build_list_etree(cls, parent, items):
+        """ Builds a element for a list of items.
+
+        :param lxml.etree.Element parent: The parent element to add to
+        :param list items: The list to build elements for
+        :return: The built list element
+        :rtype: lxml.etree.Element
+        """
+
         for item in items:
             if isinstance(item, dict):
                 dict_element = etree.Element(parent.tag)
-                parent.append(cls._build_dict_etree(item, dict_element))
+                parent.append(cls._build_dict_etree(dict_element, item))
             elif isinstance(item, (list, tuple, set, frozenset)):
                 parent.append(cls._build_list_etree(parent, item))
             else:
@@ -61,28 +104,64 @@ class XMLParser(object):
         return parent
 
     @classmethod
-    def _build_dict_etree(cls, dictionary, tree):
-        # TODO: compress _build_list_etree and _build_dict_etree logic
+    def _build_dict_etree(cls, parent, dictionary):
+        """ Builds a element for a dictionary.
+
+        :param lxml.etree.Element: The parent element to add to
+        :param dict dictionary: The dictionary to build from
+        :return: The built dictionary element
+        :rtype: lxml.etree.Element
+        """
+
         for (key, value) in dictionary.items():
             if isinstance(value, dict):
-                tree.append(cls._build_dict_etree(value, etree.Element(key)))
+                parent.append(cls._build_dict_etree(etree.Element(key), value))
             elif isinstance(value, (list, tuple, set, frozenset)):
                 list_parent = etree.Element(key)
-                tree.append(cls._build_list_etree(list_parent, value))
+                parent.append(cls._build_list_etree(list_parent, value))
             else:
-                tree.append(cls._build_base_etree(tree, key, value))
-        return tree
+                parent.append(cls._build_base_etree(parent, key, value))
+        return parent
 
     @classmethod
     def from_dict(cls, dictionary, root="root"):
-        return cls(cls._build_dict_etree(dictionary, etree.Element(root)))
+        """ Create an instance of ``XMLParser`` from a given dictionary.
+
+        :param dict dictionary: The dictionary to build from
+        :param root: The name of the root element to use, defaults to "root", optional
+        :return: The new ``XMLParser`` instance
+        :rtype: XMLParser
+        """
+
+        return cls(cls._build_dict_etree(etree.Element(root), dictionary))
 
     @classmethod
     def from_xml(cls, content):
-        return cls(etree.XML(content))
+        """ Create an instance of ``XMLParser`` from some xml content.
 
-    def to_dict(self):
-        return self._build_dict(self.tree)
+        :param str content: The xml content to build from
+        :return: The new ``XMLParser`` instance
+        :rtype: XMLParser
+        """
+
+        return cls(etree.fromstring(content))
+
+    def to_dict(self, dict_type=collections.OrderedDict):
+        """ Get the dictionary representation of the current parser.
+
+        :param class dict_type: The dictionary type to build
+        :return: The resulting dictionary
+        :rtype: dict
+        """
+
+        return self._build_dict(self.tree, dict_type=dict_type)
 
     def to_xml(self, pretty=False):
+        """ Get the xml string of the current parser.
+
+        :param bool pretty: Pretty format the resulting xml string
+        :return: The xml string of the current parser
+        :rtype: str
+        """
+
         return etree.tostring(self.tree, pretty_print=pretty).decode("utf-8")
--- src/file_config/handlers/xml.py
+++ src/file_config/handlers/xml.py
@@ -6,12 +6,27 @@ from ..contrib import XMLParser
 
 
 class XMLHandler(BaseHandler):
+    """ The XML serialization handler.
+    """
 
     name = "xml"
     packages = ("lxml",)
     options = {"root": None, "pretty": False}
 
     def on_lxml_dumps(self, lxml, config, dictionary, **kwargs):
+        """ The `lxml <https://pypi.org/project/lxml/>`_ dumps method.
+
+        :param module lxml: The ``lxml`` module
+        :param class config: The instance's config class
+        :param dict dictionary: The dictionary to serialize
+        :param str root: The root tag of the xml document, defaults to the config
+            instance's name, optional
+        :param bool pretty: Pretty format the resulting xml document, defaults to
+            False, optional
+        :returns: The XML serialization
+        :rtype: str
+        """
+
         root = kwargs.pop("root")
         if not isinstance(root, str):
             root = config.__name__
@@ -21,4 +36,13 @@ class XMLHandler(BaseHandler):
         )
 
     def on_lxml_loads(self, lxml, config, content, **kwargs):
+        """ The `lxml <https://pypi.org/project/lxml/>`_ loads method.
+
+        :param module lxml: The ``lxml`` module
+        :param class config: The loading config class
+        :param str content: The content to deserialize
+        :returns: The deserialized dictionary
+        :rtype: dict
+        """
+
         return XMLParser.from_xml(content).to_dict()
