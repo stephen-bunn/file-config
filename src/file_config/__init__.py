724be6cdc54f738709382f1f7c2bced6e5518e52
Stephen Bunn <stephen@bunn.io>
2018-12-14 11:03:33 -0500

    Sorting imports
    Signed-off-by: Stephen Bunn <stephen@bunn.io>

--- src/file_config/__init__.py
+++ src/file_config/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2018 Stephen Bunn <stephen@bunn.io>
 # ISC License <https://choosealicense.com/licenses/isc>
 
-from . import handlers, contrib, __version__
+from . import contrib, handlers, __version__
 from .constants import CONFIG_KEY
 from ._file_config import var, config, to_dict, validate, from_dict, make_config
 from .schema_builder import Regex, build_schema
