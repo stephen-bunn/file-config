fed279434f54b135d7cb7ddf960893ad2c11d6ac
Stephen Bunn <stephen@bunn.io>
2018-12-16 14:03:21 -0500

    Bumping version to 0.3.0
    Signed-off-by: Stephen Bunn <stephen@bunn.io>

--- src/file_config/__version__.py
+++ src/file_config/__version__.py
@@ -3,7 +3,7 @@
 
 __name__ = "file-config"
 __repo__ = "https://github.com/stephen-bunn/file-config"
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 __description__ = "An attr's based configuration file abstraction"
 __author__ = "Stephen Bunn"
 __contact__ = "stephen@bunn.io"
