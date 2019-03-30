8756268b926b62ac50c560e0458f4cf9a4f56037
Stephen Bunn <stephen@bunn.io>
2019-03-30 00:35:34 -0400

    Adding yaml reflective tests

--- tests/strategies.py
+++ tests/strategies.py
@@ -11,6 +11,7 @@ from hypothesis import assume
 from hypothesis.strategies import (
     one_of,
     from_regex,
+    from_type,
     composite,
     builds,
     none,
@@ -44,13 +45,13 @@ MAX_CONFIG_VARS = 5
 
 
 @composite
-def builtins(draw, ignore=None):
+def builtins(draw, ignore=None, allow_nan=True):
     return draw(
         one_of(
             none(),
             integers(),
             booleans(),
-            floats(),
+            floats(allow_nan=False),
             builds(tuple),
             builds(list),
             builds(set),
@@ -58,7 +59,7 @@ def builtins(draw, ignore=None):
             characters(),
             text(),
             binary(),
-            complex_numbers(),
+            # complex_numbers(allow_nan=allow_nan),
         )
     )
 
@@ -78,29 +79,38 @@ def variable_name(draw):
 
 
 @composite
-def config_var(draw, allowed_strategies=None):
+def config_var(draw, allowed_strategies=None, allow_nan=True):
     if isinstance(allowed_strategies, list):
         value = draw(one_of(allowed_strategies))
     else:
-        value = draw(builtins())
+        value = draw(builtins(allow_nan=allow_nan))
     return file_config.var(type=type(value), default=value)
 
 
 @composite
-def config_var_dict(draw, min_vars=None, max_vars=None, allowed_strategies=None):
+def config_var_dict(
+    draw, min_vars=None, max_vars=None, allowed_strategies=None, allow_nan=True
+):
     if not isinstance(min_vars, int):
         min_vars = MIN_CONFIG_VARS
     if not isinstance(max_vars, int):
         max_vars = MAX_CONFIG_VARS
     return {
-        draw(variable_name()): draw(config_var(allowed_strategies=allowed_strategies))
+        draw(variable_name()): draw(
+            config_var(allowed_strategies=allowed_strategies, allow_nan=allow_nan)
+        )
         for _ in range(MIN_CONFIG_VARS, random.randint(min_vars, max_vars) + 1)
     }
 
 
 @composite
 def config(
-    draw, config_vars=None, min_vars=None, max_vars=None, allowed_strategies=None
+    draw,
+    config_vars=None,
+    min_vars=None,
+    max_vars=None,
+    allowed_strategies=None,
+    allow_nan=True,
 ):
     if isinstance(config_vars, dict):
         return file_config.make_config(draw(class_name()), config_vars)
@@ -112,9 +122,37 @@ def config(
                     min_vars=min_vars,
                     max_vars=max_vars,
                     allowed_strategies=allowed_strategies,
+                    allow_nan=allow_nan,
                 )
             ),
         )
+
+
+@composite
+def config_instance(
+    draw,
+    config_vars=None,
+    min_vars=None,
+    max_vars=None,
+    allowed_strategies=None,
+    allow_nan=True,
+):
+    config_class = draw(
+        config(
+            config_vars=config_vars,
+            min_vars=min_vars,
+            max_vars=max_vars,
+            allowed_strategies=allowed_strategies,
+            allow_nan=allow_nan,
+        )
+    )
+    config_vars = {}
+    for key, value in attr.fields_dict(config_class).items():
+        var_value = draw(from_type(value.type))
+        if not allow_nan and isinstance(var_value, float):
+            assume(not math.isnan(var_value))
+        config_vars[key] = var_value
+    return config_class(**config_vars)
 
 
 @composite
